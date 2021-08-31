455c3aee47a6a355e45d9ffc0eaf91ccfa825796
Xayme <lalarcon@clinicarequipa.com.pe>
2021-08-30 23:41:50 -0500

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+const GifExpertApp = () => {
+  //const categories = ["Schnauzer", "French Bulldog", "Pug"];
+  const [categories, setCategories] = useState([
+    "Schnauzer",
+    //    "French Bulldog",
+    //    "Pug",
+  ]);
+
+  //const handleAdd = () => {
+  //  setCategories([...categories, "Choloscuincle"]);
+  //};
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GifExpertApp;
